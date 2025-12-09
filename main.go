be4f6e7e724b2690894b8ee910811efcf392a18c
Elliott Minns <elliott.minns@pm.me>
2025-12-08 22:25:44 -0600

    feat: working code

--- main.go
+++ main.go
@@ -1,1 +1,37 @@
 package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"os/signal"
+
+	"github.com/jackc/pgx/v5"
+)
+
+func main() {
+	dbURL := os.Getenv("DATABASE_URL")
+	if dbURL == "" {
+		fmt.Fprintln(os.Stderr, "DATABASE_URL environment variable is required")
+		os.Exit(1)
+	}
+
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
+
+	conn, err := pgx.Connect(ctx, dbURL)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to connect to database: %v\n", err)
+		os.Exit(1)
+	}
+	defer conn.Close(ctx)
+
+	var count int
+	err = conn.QueryRow(ctx, "SELECT COUNT(*) FROM public.users").Scan(&count)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to query users count: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Println("users: ", count)
+}
